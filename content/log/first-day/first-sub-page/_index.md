d9a6a64cb889fd8ffbadea7a8a46722107652cf5
addmecode <am.adrian.oth@gmail.com>
2026-01-09 22:28:50 +0100

    init

---
+++ content/log/first-day/first-sub-page/_index.md
@@ -0,0 +1,4 @@
++++
+date = '2026-01-09T21:18:48+01:00'
+title = 'First Day subpage'
++++
---
+++ public/searchindex.en.js
@@ -0,0 +1,58 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "My notes",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "Log",
+    "uri": "/log/index.html"
+  },
+  {
+    "breadcrumb": "My notes \u003e Log",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Third Day",
+    "uri": "/log/third-day/index.html"
+  },
+  {
+    "breadcrumb": "My notes \u003e Log",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Second Day",
+    "uri": "/log/second-day/index.html"
+  },
+  {
+    "breadcrumb": "My notes \u003e Log",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "First Day",
+    "uri": "/log/first-day/index.html"
+  },
+  {
+    "breadcrumb": "My notes \u003e Log \u003e First Day",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "First Day subpage",
+    "uri": "/log/first-day/first-sub-page/index.html"
+  },
+  {
+    "breadcrumb": "My notes",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My notes",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
